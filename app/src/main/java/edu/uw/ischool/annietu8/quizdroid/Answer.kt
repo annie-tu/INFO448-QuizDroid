594da3972ec2e6de77efbaf85ba61e1c06540b97
annie-tu <annietu18@gmail.com>
2023-11-07 16:13:10 -0800

    start project

---
+++ app/src/main/java/edu/uw/ischool/annietu8/quizdroid/Answer.kt
@@ -0,0 +1,11 @@
+package edu.uw.ischool.annietu8.quizdroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Answer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answer)
+    }
+}
---
+++ app/src/main/java/edu/uw/ischool/annietu8/quizdroid/MainActivity.kt
@@ -0,0 +1,34 @@
+package edu.uw.ischool.annietu8.quizdroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var topicListView : ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val topics = arrayOf(
+            "Math",
+            "Physics",
+            "Marvel Superheroes")
+
+        topicListView =  findViewById(R.id.topicListView)
+        topicListView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, topics)
+        topicListView.onItemClickListener = AdapterView.OnItemClickListener {
+                parent, view, position, id ->
+            Toast.makeText(this, "You selected ${position}: ${topics[position]}", Toast.LENGTH_SHORT).show()
+            // do something
+            // val intent = Intent(this, TopicOverviewActivity::class.java)
+            //startActivity(intent)
+            //finish()
+        }
+    }
+}
